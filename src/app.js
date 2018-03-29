8a1d4b03748ee7b887a6a8cf9fc199ce79711642
Saigal <saigal.amatya@gmail.com>
2018-03-29 21:16:00 +0545

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// import './utils.js';
+// import { square, add } from './utils';
+import { isAdult, canDrink } from './person';
+
+console.log('app.js is running!');
+// console.log(square(5));
+// console.log(add(2, 3));
+console.log(isAdult(19));
+console.log(canDrink(22));
